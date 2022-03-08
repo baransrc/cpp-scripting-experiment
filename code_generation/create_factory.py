4a20271b8943e7b787c7bd1da9cc5dd04294ac6d
Baran Surucu <baransrc@gmail.com>
2022-03-08 03:53:49 +0100

    Added nullptr return to the ScriptFactory InstantiateScript function generator.

--- code_generation/create_factory.py
+++ code_generation/create_factory.py
@@ -9,6 +9,9 @@ scripting_namespace = 'Gameplay::'
 generated_includes = '#include \"scripting_pch.h\"\n' + '#include \"ScriptFactory.h\"\n' 
 generated_body = scripting_namespace + 'Script* InstantiateScript(Gameplay::Entity* script_owner, const std::string& script_name)\n{'
 
+print("Running Pre-Build Event \"Create ScriptFactory\"")
+print("Generating ScriptFactory function using header files:")
+
 for file_name in files_in_directory:
     if file_name[-2:] == '.h':
         header_file = open(path+file_name)
@@ -18,11 +21,12 @@ for file_name in files_in_directory:
         is_script_file = header_file_as_string_without_spaces.find('class'+class_name+':publicScript') != -1
 
         if is_script_file:
-            print("Found script header file: " + file_name)
+            print(file_name)
             generated_includes += '#include \"' + file_name + '\"\n'
             generated_body += '\n\tif (script_name == \"' + class_name + '\")\n\t{\n\t\treturn new ' + scripting_namespace + class_name + '(script_owner);\n\t}\n'
-            
-generated_body += '}'
+
+
+generated_body += '\n\treturn nullptr;\n}'
 
 generated_body = generated_includes + '\n\n' + generated_body
 
